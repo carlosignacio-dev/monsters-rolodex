1fa29928a1086c7c7f22b5e95eab8c204668a066
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-06-11 14:15:30 -0400

    added optimizations

--- src/App.js
+++ src/App.js
@@ -9,30 +9,51 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-        name: "linda",
-      },
-      {
-        name: "Frank",
-      },
-      {
-        name: "Jacky"
-      },
-      {
-        name: "Andrei"
-      },
-    ]
-      
+      monsters: [], 
+      searchField: ""     
+  };
+}
+
+componentDidMount() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
     }
-  }
+    ))
+}
+
+onSearchChange = (event) => {
+  const searchField = event.target.value.toLowerCase(); 
+  this.setState(() => {
+    return {searchField};
+  });
+}
 
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>;
+        <input 
+        className="search-box" 
+        type="search" 
+        placeholder="Search Monsters" 
+        onChange={onSearchChange}/>
+        {filteredMonsters.map((monster) => {
+            return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+            );
           })
         }
       </div>
